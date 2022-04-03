1d6811db3a3f7c2a6f3b0fef4a34fa03078e221a
Nicolas <nicolas26005@gmail.com>
2022-04-03 18:47:08 +0200

    Beginning of heap

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,82 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 
+/**
+ * 
+ * BinaryHeap using a binary tree where the minimum element is always the root
+ *
+ * @param <T>
+ */
 class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+	private ArrayList<T> binaryTree;
+	private Comparator<T> compare;
 
-    public T pop() { return null; }
+	public BinaryHeap(Comparator<T> comparator) {
+		binaryTree = new ArrayList<T>();
+		compare = comparator;
+	}
 
-    public T peek() { return null; }
+	public T pop() {
+		return null;
+	}
 
-    public void push(T element) {  }
+	/**
+	 * Return the minimum element of the binary heap
+	 * 
+	 * @return the minimum T object
+	 */
+	public T peek() {
+		if (binaryTree.isEmpty()) {
+			return null;
+		}
+		return binaryTree.get(0);
+	}
 
-    public int count() { return 0; }
+	public void push(T element) {
+		int parentIndex=(int)Math.floor((this.count()-1)/2);
+		
+		if (binaryTree.isEmpty()) {
+			binaryTree.add(element);
+
+		} else if (compare.compare(element, binaryTree.get((int)Math.floor((this.count()-1)/2))) >= 1) {
+			// if the element to add is greater or equals than its parent,
+			// then it is added because it is still ordered
+			binaryTree.add(element);
+			
+		}else {
+			
+			while(parentIndex>-1) {
+				T current = binaryTree.get(parentIndex);
+				int childIndex= (2*parentIndex)+1;
+				if (compare.compare(element, current) < 1) {
+					binaryTree.set(parentIndex, element);
+					
+					if(childIndex>=this.count()) {
+						binaryTree.add(current);
+					}else {
+						binaryTree.set(childIndex, current);
+					}
+					
+				}
+				parentIndex=(int)Math.floor((parentIndex-1)/2);
+
+			}
+			
+		}
+
+	}
+
+	/**
+	 * Returns the number of elements in the binary tree.
+	 * 
+	 * @return
+	 */
+	public int count() {
+		return binaryTree.size();
+	}
 
 }
--- code/tp3-heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/tp3-heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -4,7 +4,43 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 class BinaryHeapTest {
 
-
+	BinaryHeap<Integer> tree;
+	ComparatorSuperior comp;
+	@BeforeEach
+	public void setUp() {
+		comp = new ComparatorSuperior();
+		tree = new BinaryHeap<Integer>(comp);
+	}
+	
+	@Test
+	public void pushInEmptyTest() {
+		tree.push(12);
+		assertEquals(tree.count(), 1);
+	}
+	
+	@Test
+	public void pushTwoInOrderTest() {
+		tree.push(12);
+		tree.push(134);
+		assertEquals(tree.count(), 2);
+	}
+	
+	@Test
+	public void pushTwoInDisorderTest() {
+		tree.push(12);
+		tree.push(134);
+		assertEquals(tree.count(), 2);
+	}
+	
+	@Test
+	public void getMinTest() {
+		tree.push(1422);
+		tree.push(1);
+		assertEquals(tree.peek(), 1);
+	}
 }
